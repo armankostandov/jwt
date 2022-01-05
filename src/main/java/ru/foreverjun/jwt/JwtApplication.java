3c3495b6a837f7f5231f1b55c4dccb90e0c4dd2c
Arman Kostandov <armankostandov@gmail.com>
2022-01-05 14:42:56 +0300

    bootstrap data

--- src/main/java/ru/foreverjun/jwt/JwtApplication.java
+++ src/main/java/ru/foreverjun/jwt/JwtApplication.java
@@ -1,7 +1,14 @@
 package ru.foreverjun.jwt;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import ru.foreverjun.jwt.model.Role;
+import ru.foreverjun.jwt.model.User;
+import ru.foreverjun.jwt.service.UserService;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class JwtApplication {
@@ -10,4 +17,26 @@ public class JwtApplication {
 		SpringApplication.run(JwtApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(UserService userService) {
+		return args -> {
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_MANAGER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+			userService.saveUser(new User(null, "John Travolta", "john", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Will Smith", "will", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Jim Carry", "jim", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Arnold Schwarzenegger", "arnold", "1234", new ArrayList<>()));
+
+			userService.addRoleToUser("john", "ROLE_USER");
+			userService.addRoleToUser("john", "ROLE_MANAGER");
+			userService.addRoleToUser("will", "ROLE_MANAGER");
+			userService.addRoleToUser("jim", "ROLE_ADMIN");
+			userService.addRoleToUser("arnold", "ROLE_SUPER_ADMIN");
+			userService.addRoleToUser("arnold", "ROLE_ADMIN");
+			userService.addRoleToUser("arnold", "ROLE_USER");
+		};
+	}
 }
